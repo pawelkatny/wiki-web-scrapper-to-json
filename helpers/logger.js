f13a747a3317f2f75e0bfb5c376b88d4fbfbf806
Paweł Kątny <pawel.katny@gmail.com>
2023-05-08 21:47:20 +0200

    Add logger
    * show current progress as a bar
    * write message to CLI

---
+++ helpers/logger.js
@@ -0,0 +1,44 @@
+const process = require("node:process");
+
+const MAX_PROGRESS_BAR_LENGTH = 50;
+
+class Logger {
+  constructor(noRecords) {
+    this.progress = {
+      noRecords: noRecords,
+      current: 0,
+      bar: "",
+    };
+  }
+
+  showProgress(current) {
+    this.progress.current = current;
+    const progressPercent = Math.round(
+      (current / this.progress.noRecords) * 100
+    );
+    const currentBarLength = Math.round(
+      (progressPercent / 100) * MAX_PROGRESS_BAR_LENGTH
+    );
+    const emptyBarSpace = MAX_PROGRESS_BAR_LENGTH - currentBarLength;
+
+    const progressBar = `[${"=".repeat(currentBarLength)}${
+      currentBarLength != MAX_PROGRESS_BAR_LENGTH ? ">" : ""
+    }${" ".repeat(emptyBarSpace)}] ${progressPercent.toFixed(2)}%\n`;
+    this.progress.bar = progressBar;
+
+    process.stdout.write(progressBar);
+  }
+
+  log(msg) {
+    process.stdout.write(msg + "\n");
+  }
+
+  clearLine(n) {
+    for (let i = 0; i < n; i++) {
+      process.stdout.moveCursor(0, -1);
+      process.stdout.clearLine(0);
+    }
+  }
+}
+
+module.exports = Logger;
