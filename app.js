372f856b68c1649079cd262e7cba375bcc3eea0f
Paweł Kątny <pawel.katny@gmail.com>
2023-05-30 21:37:57 +0200

    Add write stringified data to file

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const Logger = require("./helpers/logger");
+const fs = require("fs");
+const path = require("path");
 
 const DATA_DIR = "data";
 const WIKI_API_URI = "https://pl.wikipedia.org/wiki";
@@ -17,13 +19,15 @@ const WIKI_HERBS_LIST_URI =
   logger.log(`Found ${links.length} wiki links...`);
   logger.log("Starting scrapping procedure...");
   logger.log("");
-
+  let counter = 0;
   for (const prop in links) {
     const current = linksKeys.indexOf(prop);
     const hrefRegex = new RegExp("/wiki/*");
     const link = links[prop].attribs;
 
     if (hrefRegex.test(link?.href)) {
+      if (counter >= 1) return;
+      counter++;
       logger.showProgress(current + 1);
       logger.log(`Loading HTML content for WIKI page: "${link.title}"`);
       const wikiPageUri = `${WIKI_API_URI}/${link.href.replace("/wiki/", "")}`;
@@ -66,6 +70,8 @@ const WIKI_HERBS_LIST_URI =
           container
         );
       });
+
+      saveParsedDataToJSON(parsedData);
     }
   }
 
@@ -84,4 +90,22 @@ const getParagraphDataRecursive = ($, ele, parsedData, title, container) => {
   };
 
   return getParagraphDataRecursive($, e, parsedData, title, container);
+};
+
+const saveParsedDataToJSON = (data) => {
+  const stringifiedData = JSON.stringify(data);
+  const jsonFileName = data.title.toLowerCase().replace(/[ ]/, "_") + ".json";
+  const dir = path.join(process.cwd(), DATA_DIR);
+  const file = path.join(dir, jsonFileName);
+
+  if (fs.e)
+    fs.mkdir(dir, { recursive: false }, (err) => {
+      if (err.code != "EEXIST") {
+        console.log(err);
+      }
+    });
+
+  fs.writeFile(file, stringifiedData, { flag: "w" }, (err) => {
+    console.log(err);
+  });
 };
